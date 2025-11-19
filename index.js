43635f9f87c050033249dcb1ea4cde1a2271861a
gaega nooson <gaega@gaegas-MacBook-Air.local>
2025-11-18 19:39:38 -0600

    Implemented weather app

---
+++ index.js
@@ -0,0 +1,42 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const input = document.getElementById("weather-input");
+    const button = document.getElementById("weather-button");
+
+    button.addEventListener("click", (e) => {
+        e.preventDefault();
+        const city = input.value.trim();
+
+        if (!city) return alert("Please enter a city name.");
+
+
+        const API_KEY = `19f5d9869bf3cb8e271bbfad6a388c5e`
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=Imperial&appid=${API_KEY}`;
+
+        fetch(url)
+        .then((res) => res.json())
+        .then((data)=>{
+            if (data.cod !== 200){
+                alert("City not found");
+                return;
+            }
+
+            const weather = {
+                city: data.name, 
+                country: data.sys.country,
+                temp: data.main.temp,
+                desc: data.weather[0].description,
+                icon: `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`,
+
+            };
+
+            console.log(weather);
+
+            document.getElementById("city-output").textContent = `${weather.city}, ${weather.country}`;
+document.getElementById("temp-output").textContent = `${weather.temp} °F`;
+document.getElementById("weather-display").style.display = "block";
+        })
+        .catch((err)=>{
+            alert("Error fetching weather!");
+        });
+    });
+});
